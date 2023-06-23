60abd067f5ee1793b524a0f34e6acecfb050e03b
Caleb-Komen <calebkipyatich@gmail.com>
2023-06-24 00:08:43 +0300

    Update theme colours.

--- app/src/main/java/com/techdroidcentre/music/ui/theme/Color.kt
+++ app/src/main/java/com/techdroidcentre/music/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.techdroidcentre.music.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val darkPrimary = Color(0xFF394D5F)
+val darkOnPrimary = Color(0xFFFFFFFF)
+val darkSecondary = Color(0xFF4C6479)
+val darkOnSecondary = Color(0xFFFFFFFF)
+val darkTertiary = Color(0xFF4C6479)
+val darkOnTertiary = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val lightPrimary = Color(0xFF394D5F)
+val lightOnPrimary = Color(0xFFFFFFFF)
+val lightSecondary = Color(0xFF44515C)
+val lightOnSecondary = Color(0xFFFFFFFF)
+val lightTertiary = Color(0xFF44515C)
+val lightOnTertiary = Color(0xFFFFFFFF)
--- app/src/main/java/com/techdroidcentre/music/ui/theme/Theme.kt
+++ app/src/main/java/com/techdroidcentre/music/ui/theme/Theme.kt
@@ -16,25 +16,21 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = darkPrimary,
+    onPrimary = darkOnPrimary,
+    secondary = darkSecondary,
+    onSecondary = darkOnSecondary,
+    tertiary = darkTertiary,
+    onTertiary = darkOnTertiary,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = lightPrimary,
+    onPrimary = lightOnPrimary,
+    secondary = lightSecondary,
+    onSecondary = lightOnSecondary,
+    tertiary = lightTertiary,
+    onTertiary = lightOnTertiary,
 )
 
 @Composable
