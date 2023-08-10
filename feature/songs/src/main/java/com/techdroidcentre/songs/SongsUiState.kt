5183ff497dd49a36df702cc8614e347a8ccc6fbc
Caleb-Komen <calebkipyatich@gmail.com>
2023-08-10 22:47:37 +0300

    Enable sorting songs by title and artist.

--- feature/songs/src/main/java/com/techdroidcentre/songs/SongsUiState.kt
+++ feature/songs/src/main/java/com/techdroidcentre/songs/SongsUiState.kt
@@ -1,5 +1,6 @@
 package com.techdroidcentre.songs
 
+import com.techdroidcentre.data.datastore.SongsSortOption
 import com.techdroidcentre.model.Song
 
 data class SongsUiState(
@@ -7,5 +8,6 @@ data class SongsUiState(
     val playingSongId: String = "",
     val error: String = "",
     val loading: Boolean = false,
-    val isSongPlaying: Boolean = false
+    val isSongPlaying: Boolean = false,
+    val sortOption: SongsSortOption = SongsSortOption.TITLE
 )
